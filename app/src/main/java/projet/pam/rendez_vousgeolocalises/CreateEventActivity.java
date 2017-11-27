4e9b1f582a9497fabab5e8112703af4ff389216d
kamneo <alexandre.pinero@gmail.com>
2017-11-27 13:01:09 +0100

    first commit

---
+++ app/src/main/java/projet/pam/rendez_vousgeolocalises/CreateEventActivity.java
@@ -0,0 +1,14 @@
+package projet.pam.rendez_vousgeolocalises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
